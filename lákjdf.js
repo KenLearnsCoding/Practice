c6484c2bf47f4b452e22d867d7d76ec1edd58a8c
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-11-09 22:12:12 -0500

    push

--- lákjdf.js
+++ lákjdf.js
@@ -1,217 +1,60 @@
-*,
-*:after,
-*:before {
-	box-sizing: border-box;
-}
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Apple Style  Blow Out Text with CSS Scroll-Driven Animations 🍏</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="./style.css">
 
-body {
-	display: grid;
-	place-items: center;
-	min-height: 100vh;
-	font-family:  "SF Pro Text", "SF Pro Icons", "AOS Icons", "Helvetica Neue", Helvetica, Arial, sans-serif, system-ui;
-	color: hsl(0 0% 100%);
-	overflow-x: hidden;
-}
-
-section {
-	min-height: 100vh;
-	width: 100vw;
-	view-timeline-name: --section;
-}
-
-section:nth-of-type(1),
-section:nth-of-type(3) {
-	background: hsl(0 0% 100%);
-}
-
-main {
-	overflow: auto;
-	overflow-x: hidden;
-	height: 100vh;
-	width: 100vw;
-/*	scale: 0.5;*/
-	outline: 1rem dashed red;
-}
-
-h2 {
-	font-size: clamp(2rem, 3vw + 1rem, 8rem);
-}
-
-.section__content {
-	min-height: 100vh;
-	width: 100vw;
-	display: grid;
-	place-items: center;
-	position: sticky;
-	top: 0;
-	overflow: hidden;
-}
-
-section:nth-of-type(1),
-section:nth-of-type(3) {
-	color: hsl(0 0% 0%);
-}
-section:nth-of-type(1) .section__content {
-	overflow: hidden;
-}
-
-section:nth-of-type(2) {
-  z-index: 2;
-	color: hsl(0 0% 0%);
-	min-height: 200vh;
-	background: hsl(0 0% 0%);
-}
-
-section:nth-of-type(2) p {
-	--opacity: 1;
-	color: hsl(0 0% 100%);
-	font-size: clamp(1.625rem, 2vw + 1rem, 8rem);
-  letter-spacing: 0;
-  font-weight: bold;
-  line-height: 1;
-  width: 30ch;
-  max-width: calc(100% - 4rem);
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  translate: -50% -50%;
-  opacity: 0;
-	-webkit-animation: fade-in both linear, fade-out both linear;
-	        animation: fade-in both linear, fade-out both linear;
-	animation-timeline: --section;
-	animation-range: entry 10% entry 35%, exit 0% exit 25%;
-}
-
-section:nth-of-type(1) svg {
-	--opacity: 1;
-	-webkit-animation: blow-out both ease-in, fade-in both ease-in;
-	        animation: blow-out both ease-in, fade-in both ease-in;
-	font-weight: 600;
-	animation-timeline: --section;
-	animation-range: exit-crossing 10% exit 0%, exit-crossing 10% exit-crossing 25%;
-	font-size: clamp(2rem, 10vw + 1rem, 10rem);
-	transform-origin: 50% 50%;
-	width: 100%;
-	height: 100%;
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	opacity: 0.4;
-	translate: -50% -50%;
-	overflow: hidden;
-}
-
-section:nth-of-type(3) {
-  z-index: 4;
-}
-
-section:nth-of-type(3) svg {
-	width: 30vmin;
-	position: absolute;
-	stroke: hsl(0 0% 0%);
-	right: 10%;
-	bottom: 10%;
-	rotate: 10deg;
-}
-
-section:nth-of-type(1) .section__content {
-	overflow: visible;
-	transform-style: preserve-3d;
-	perspective: 100vh;
-}
-
-svg text {
-	font-size: clamp(2rem, 6vw + 1rem, 6rem);
-	font-weight: 600;
-}
-
-@-webkit-keyframes blow-out {
-	to { transform: translate3d(0.05ch, 0, 99vh); }
-}
-
-@keyframes blow-out {
-	to { transform: translate3d(0.05ch, 0, 99vh); }
-}
-
-section:nth-of-type(1) {
-	height: 200vh;
-}
-
-video {
-	position: fixed;
-	inset: 0;
-	-o-object-fit: cover;
-	   object-fit: cover;
-	width: 100%;
-	height: 100vh;
-	filter: saturate(1) brightness(1);
-	opacity: 0;
-	-webkit-animation: fade-in both linear, fade-out both linear;
-	        animation: fade-in both linear, fade-out both linear;
-	animation-timeline: --section;
-	animation-range: entry 0% entry 25%, exit 10% exit 35%;
-}
-
-@-webkit-keyframes fade-in {
-	to {
-		opacity: var(--opacity, 0.2);
-	}
-}
-
-@keyframes fade-in {
-	to {
-		opacity: var(--opacity, 0.2);
-	}
-}
-
-@-webkit-keyframes fade-out {
-	to {
-		opacity: 0;
-	}
-}
-
-@keyframes fade-out {
-	to {
-		opacity: 0;
-	}
-}
-
-nav {
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	border-bottom: 1px solid hsl(0 0% 50%);
-	height: 52px;
-	background: hsl(0 0% 20% / 0.75);
-	-webkit-backdrop-filter: blur(4px);
-	        backdrop-filter: blur(4px);
-	z-index: 5;
-}
-
-
-.nav__content {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	width: 800px;
-	max-width: 100vh;
-	padding: 0 1rem;
-	margin: 0 auto;
-	height: 100%;
-}
-
-nav a {
-	text-decoration: none;
-	background: hsl(210 100% 45%);
-	color: hsl(0 0% 98%);
-	font-weight: 400;
-	padding: 0.5rem 1rem;
-	border-radius: 100px;
-	font-size: 0.875rem;
-}
-
-nav img {
-	height: 50%;
-	justify-self: start;
-}
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<nav>
+      <div class="nav__content">
+        <img
+          src="https://assets.codepen.io/605876/bear-2022--white.png"
+          alt=""
+        />
+        <a
+          href="https://twitter.com/intent/follow?screen_name=jh3yy"
+          target="_blank"
+          >Follow</a
+        >
+      </div>
+    </nav>
+    <section>
+      <div class="section__content">
+        <!-- <h1>Your Name</h1> -->
+        <svg>
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">
+            Your Name
+          </text>
+        </svg>
+      </div>
+    </section>
+    <section>
+      <video
+        src="https://assets.codepen.io/605876/chameleon.mp4"
+        autoplay
+        playsinline
+        muted
+        loop
+      ></video>
+      <div class="section__content">
+        <p>
+          Animations that run off the main thread. That can be driven by either
+          the scroll position of a container or an element's position within a
+          container! No JavaScript.<br />Pretty cool.
+        </p>
+      </div>
+    </section>
+    <section>
+      <div class="section__content">
+        <h2>fin.</h2>
+        <svg aria-hidden="true" class="ml-6 w-1/4" stroke="var(--text-1)" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 271 209"><title>Jhey</title><path d="M144.745 123.82c1.907-1.258-3.266-2.199-4.184-2.418-4.076-.973-15.825-2.609-16.141 4.319-.725 15.907 36.347 5.736 16.072-3.986M102.952 112.797c-5.6848 0-6.2149 7.73-.728 7.12 6.139-.682 3.185-9.905-1.861-6.877M170.025 108.347c-1.398-2.796-7.244 2.284-4.531 6.23 2.713 3.946 8.442-.486 6.149-4.612-.608-1.094-3.096-2.133-4.288-1.537M117.998 100.704c0-9.5524-14.086-13.3378-21.4395-11.3323-11.7769 3.2119-15.927 9.6813-15.927 21.1333M187.183 101.246C182.107 82.5407 155.739 77.9455 151.5 99" stroke-width="5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M40.3725 26.8984C58.6558 41.1564 141.659 43.1867 128.248 5.48254c-.337-.94488-1.163-3.24224-2.31-3.47304-1.224-.24653-4.009 4.38498-4.311 4.81425C100.965 36.1863 95.2641 73.5992 74.5923 102.644c-10.8878 15.298-59.8032 43.034-69.03244 10.837C-17.5939 32.705 78.7483 76.0672 105.741 67.4678c14.016-4.4657 19.556-16.7853 27.09-28.3056 2.387-3.6496 4.797-14.5469 7.212-10.9155 4.728 7.1114-20.401 41.6294-24.484 50.2225-4.6 9.679 13.669-31.7231 21.237-24.1359 9.433 9.4564-8.56 28.4026 16.571 7.3471 4.267-3.5745 13.215-15.2775 7.666-14.8349-7.056.563-19.468 20.1743-9.348 23.1872 9.846 2.9308 24.354-31.3131 22.327-21.2426-1.003 4.9789-5.669 18.5794 1.966 20.1168 10.559 2.1259 15.596-33.041 21.559-24.071C240.356 109.24 81.7126 283.324 50.2184 167.261 25.2159 75.1229 240.563 89.2082 268.88 137.08" stroke-width="5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+      </div>
+    </section>
+<!-- partial -->
+  
+</body>
+</html>
